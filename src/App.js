e940f3e161d64a743add339eace07e3d98329c9a
Timmydee <dahunsitimmy@gmail.com>
2022-12-22 08:05:29 -0800

    up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login/Login";
+import Login from "./pages/Login/Login";
 import Signup1 from "./pages/Signup1/Signup1";
 import Dashboard from "./pages/dashboard/Dashboard";
-import LandingPage from "./pages/landing/LandingPage";import Modal3 from "./pages/Modal3/Modal3";
+import LandingPage from "./pages/landing/LandingPage";
+import Modal3 from "./pages/Modal3/Modal3";
+import Transactions from "./pages/Transactions/Transactions";
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup1 />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Transactions/Transactions.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Transactions.css'
+
+import Layout from '../../components/layout/Layout'
+
+const Transactions = () => {
+  return (
+    <Layout>
+        <h1>
+            Transactions P
+        </h1>
+    </Layout>
+  )
+}
+
+export default Transactions
