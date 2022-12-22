d34fdb22f3063fe80d607e61226c817ac0bb3821
Timmydee <dahunsitimmy@gmail.com>
2022-12-22 06:03:21 -0800

    up

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Signup1 from "./pages/Signup1/Signup1";
+import Modal3 from "./pages/Modal3/Modal3";
 
 
 function App() {
   return (
     <div className="App">
+      <Modal3/>
       <Router>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/signup" element={<Signup1 />}/>
+          {/* <Route exact path="/" element={<Login />} /> */}
+          {/* <Route exact path="/signup" element={<Signup1 />}/> */}
 
         </Routes>
       </Router>
